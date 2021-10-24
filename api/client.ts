4c8dcc304ecbe34a556b03e80cd1a911ed7b563a
Konstantin <konstantin@grebenev.com>
2021-10-24 15:40:19 +0300

    fix: axios request fixed

---
+++ api/client.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { GetServerSidePropsContext } from 'next';
+
+const clientRequest = ({ req }: GetServerSidePropsContext) => {
+  if (typeof window === 'undefined') {
+    console.log(req);
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/',
+      headers: req.headers,
+    } as AxiosRequestConfig);
+  } else {
+    return axios.create({
+      baseURL: '/',
+    });
+  }
+};
+
+export { clientRequest };
