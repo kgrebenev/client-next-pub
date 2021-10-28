ddda130cc1c975e82d8918fd9d2dc739885b6b96
Konstantin Grebenev <konstantin@grebenev.com>
2021-10-28 09:07:30 +0300

    fix: clientRequest moved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import { AppProps } from 'next/app';
+import type { AppProps, AppContext } from 'next/app';
+
 import '../scss/styles.scss';
+import { clientRequest } from '../api/client';
 
 const myApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <h1>Header</h1>
+      <Component {...pageProps} />;
+    </div>
+  );
+};
+
+myApp.getInitialProps = async ({ ctx }: AppContext) => {
+  const client = clientRequest(ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  console.log(data);
+  return { data };
 };
 
 export default myApp;
